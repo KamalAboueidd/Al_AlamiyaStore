99c107573505b4b4d883b3b9f62183f9a7ee5084
kamal almorsy <mrsykmal@440gmail.com>
2026-04-19 23:56:43 +0200

    Setup deployment settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import './index.css'
 import './i18n.js'
 import App from './App.jsx'
@@ -8,7 +8,10 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <HashRouter>
+
       <App />
+    </HashRouter>
     </BrowserRouter>
   </StrictMode>,
 )
